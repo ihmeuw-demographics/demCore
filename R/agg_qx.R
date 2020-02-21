96f6e786e9736f45a78e320140fbf48de335ccc0
Katie Paulson <krpaul@uw.edu>
2020-02-21 13:56:54 -0800

    agg_qx rework in progress

--- R/agg_qx.R
+++ R/agg_qx.R
@@ -2,87 +2,58 @@
 #'
 #' Given a data.table with a qx variable and ID variables that uniquely
 #'   identify the data, compile granular ages and aggregate into combined qx values.
+#'   Using probability rules, we convert qx to px (conditional survival probability),
+#'   multiply these px values within a specified age range together to get the
+#'   conditional probability of survival through all granular age groups, and
+#'   subtract from one to get the aggregated conditional probability of death (qx).
 #'
-#' @param dt data.table with variables: qx, age_start, age_end, all id_cols
-#' @param age_start numeric, year of age to start at (e.g. 0 for 5q0)
-#' @param age_end numeric, year of age to end at (e.g. 5 for 5q0)
-#' @param id_cols character vector of column names in dt that, along with age_start
-#'   and age_end, uniquely identify all rows in dt
+#' @param dt data.table with data to be aggregated.
+#'   * must only include columns 'qx' and those specified in `id_cols`
+#'   * must include 'age_start' and 'age_end' columns
+#'   * `id_cols` must uniquely identify each row
+#' @param id_cols character vector of column names that uniquely identify each row
+#'   of `dt`
+#' @param target_ages_dt data.table defining age groups to aggregate to.
+#'   * must include 'age_start' and 'age_end' columns.
 #'
-#' @return data.table with id_cols and a variable called qx_#q#, where the first is
-#'   age_end - age_start, and the second is age_start
+#' @return data.table with `id_cols` and `qx` columns for aggregate age groups.
+#' @details This function is a wrapper for demUtils::aggregate_age
 #' @import data.table
 #'
 #' @examples
+#' \donotrun{
 #' dt <- data.table::data.table(
 #'   id = c(rep(1, 5), rep(2, 5)), qx = c(rep(.1, 5), rep(.2, 5)),
 #'   age_start = rep(seq(15, 35, 5), 2),
 #'   age_end = rep(seq(20, 40, 5), 2)
 #' )
-#' agg_qx(dt, age_start = 15, age_end = 40, id_cols = "id")
+#' target_dt <- data.table::data.table(age_start = c(15),
+#'                                     age_end = c(40))
+#' agg_qx(dt, id_cols = c("id", "age_start", "age_end"), target_ages_dt = target_dt)
+#' }
 #'
 #' @export
-agg_qx <- function(dt, age_start, age_end, id_cols = c()) {
+agg_qx <- function(dt, id_cols, target_ages_dt) {
 
-  # Validations and prep ----------------------------------------------------
-
-  assertable::assert_colnames(dt, c("age_start", "age_end", id_cols), only_colnames = F, quiet = T)
-  if (age_start > age_end) stop("Age start can't be larger than age end")
-  if (age_start < min(dt$age_start)) stop("Age start is lower than lowest age start in dt")
-  if (age_end > max(dt$age_end)) stop("Age end is higher than highest age end in dt")
-  if (!is.data.table(dt)) stop("The input dataset must be in data.table format")
-  if ("qx" %in% id_cols) stop("qx cannot be an id_var")
-  if ("px" %in% id_cols) stop("px cannot be an id_var")
-  if (!"qx" %in% names(dt)) stop("qx needs to be present in the dataset")
-
-  dt <- dt[, .SD, .SDcols = c(id_cols, "qx", "age_start", "age_end")]
-  setnames(dt, "age_start", "age_group_years_start")
-  setnames(dt, "age_end", "age_group_years_end")
-
-  # Calculate age length
-  dt[, age_int := age_group_years_end - age_group_years_start]
-
-  # Subset to ages of interest
-  dt <- dt[age_group_years_start >= age_start & age_group_years_end <= age_end]
-
-  # Check that you have all consecutive age groups
-  setorderv(dt, c(id_cols, "age_group_years_start"))
-  dt[, test := ifelse(age_group_years_start + age_int ==
-    shift(age_group_years_start, type = "lead"), 0, 1), by = id_cols]
-  dt[age_group_years_end == age_end, test := 0]
-  if (nrow(dt[test == 1]) > 0) stop("Need a full set of consecutive age groups")
-  if (nrow(dt[age_group_years_start == age_start]) == 0) stop("Data should include age_start")
-  if (nrow(dt[age_group_years_end == age_end]) == 0) stop("Data should include age_end")
-
-  # Calculate new qx --------------------------------------------------------
+  # Validate qx
+  assertthat::assert_that("qx" %in% names(dt), msg = "Missing 'qx' column in input")
+  assertable::assert_values(dt, c("qx"), test = "gte", test_val = 0, quiet = T)
+  assertable::assert_values(dt, c("qx"), test = "lte", test_val = 1, quiet = T)
 
   # Calculate survival probability (px)
   dt[, px := 1 - qx]
   dt[, qx := NULL]
 
-  # Collapse on the product of the transformed qx values
-  nrow_master <- nrow(dt)
-  n_ages <- length(unique(dt$age_group_years_start))
-  dt <- dt[, lapply(.SD, prod), .SDcols = "px", by = id_cols]
+  # Aggregate over age using multiplicative aggregation
+  dt <- demUtils::aggregate_age(dt,
+                                id_cols,
+                                value_cols = c("px"),
+                                target_ages_dt = target_ages_dt,
+                                value_type = "probability")
 
-  # Generate new qx based on the product of the collapsed px values
-  dt[, paste0("qx_", age_end - age_start, "q", age_start) := 1 - px]
+  # Convert px back to qx
+  dt[, qx := 1 - px]
   dt[, px := NULL]
-
-  # Check and return --------------------------------------------------------
-
-  # Check that we have lost the correct number of rows
-  # Example: if there are 5 age groups within, the resulting dataset should
-  #  be 1/5th the size of the original
-  nrow_collapse <- nrow(dt)
-  if (nrow_collapse == nrow_master) {
-    stop("You have specified an id variable that is unique within age --
-         re-check your variables")
-  }
-  if (nrow_collapse * n_ages != nrow_master) {
-    stop("The input dataset is not square (e.g. not all required age groups
-         are present for all combinations of id_cols)")
-  }
 
   return(dt)
 }
