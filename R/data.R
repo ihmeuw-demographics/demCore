7f9d46b5930d4c194c7ad055b82f5330c9075726
Katie Paulson <krpaul@uw.edu>
2020-04-02 16:43:20 -0700

    preston life table

--- R/data.R
+++ R/data.R
@@ -22,6 +22,44 @@
 "exampleLT"
 
 
+#' @title Example life table 2
+#'
+#' @description Example life table from Preston text book page 49. Post-hoc
+#'   adjustment has been made to use radix (l0) of 1 instead of 100,000, as
+#'   presented in the Preston book.
+#'
+#'   Original input data if from Austria, for males, from 1992, and comes
+#'   from the United Nations, 1994, as cited in Preston book.
+#'
+#' @format \[`data.table()`\] columns:
+#' \describe{
+#'  \item{age_start}{\[`integer()`\]\cr Abridged ages 0, 1, 5, 10, ..., 85.
+#'    This is the 'x' variable in life table notation.}
+#'  \item{age_end}{\[`integer()`\]\cr End of age group inclusive, 1, 5, 10,
+#'     ... , 85, Inf}
+#'  \item{age_length}{\[`integer()`\]\cr Width of the age interval in years.
+#'    This is the 'n' variable in life table notation.}
+#'  \item{deaths}{\[`numeric()`\]\cr Death counts in interval}
+#'  \item{population}{\[`numeric()`\]\cr Population counts in interval}
+#'  \item{mx}{\[`numeric()`\]\cr Mortality rate (deaths / population)}
+#'  \item{ax}{\[`numeric()`\]\cr Mean person-years lived by those who die in
+#'    interval.}
+#'  \item{qx}{\[`numeric()`\]\cr Probability of death in interval conditional
+#'    on survival to xth birthday.}
+#'  \item{px}{\[`numeric()`\]\cr Probability of survival in interval.}
+#'  \item{lx}{\[`numeric()`\]\cr Proportion surviving to age x.}
+#'  \item{dx}{\[`numeric()`\]\cr Proportion of cohort dying in age interval
+#'    x to x+n.}
+#'  \item{nLx}{\[`numeric()`\]\cr Person-years lived between x and x+n per
+#'    every one person in the cohort at birth.}
+#'  \item{Tx}{\[`numeric()`\]\cr Person-years lived beyond age x per every one
+#'    person in the cohort at birth.}
+#'  \item{ex}{\[`numeric()`\]\cr Life expectancy.}
+#' }
+#' @examples
+#' exampleLT2
+"exampleLT2"
+
 
 #' Regression parameters for abridged to full life table conversion
 #'
---
+++ data-raw/exampleLT2.R
@@ -0,0 +1,67 @@
+# Example life table from Preston book pg 49
+# Austria, males, 1992
+# Original data source cited by Preston: United Nations, 1994
+
+# Combination of copied-over values and calculated values. When values are
+# calculated, they are cross-referenced with the values in the book to confirm
+# equivalency.
+
+# Use for testing because generation does not require running functions from
+# this package.
+
+# age info: age (x) and age length (n)
+age_start <- c(0, 1, seq(5, 85, 5))
+age_length <- c(1, 4, rep(5, 16), Inf)
+age_end <- age_start + age_length
+
+# population (nNx) and deaths (nDx)
+pop <- c(47925, 189127, 234793, 238790, 254996, 326831, 355086, 324222, 269963,
+         261971, 238011, 261612, 181385, 187962, 153832, 105169, 73694, 57512,
+         32248)
+deaths <- c(419, 70, 36, 46, 249, 420, 403, 441, 508, 769, 1154, 1866, 2043,
+            3496, 4366, 4337, 5279, 6460, 6146)
+
+# mx
+mx <- deaths / pop
+
+# ax
+ax <- c(0.068, 1.626, 2.5, 3.143, 2.724, 2.520, 2.481, 2.601, 2.701, 2.663,
+        2.698, 2.676, 2.645, 2.624, 2.619, 2.593, 2.518, 2.423, 5.247)
+
+# qx
+qx <- (age_length * mx) / (1 + (age_length - ax) * mx)
+qx[19] <- 1
+
+# px
+px <- 1 - qx
+
+# lx
+lx <- c(100000, 99133, 98986, 98910, 98815, 98334, 97704, 97151, 96492, 95588,
+        94195, 91937, 88711, 83845, 76377, 66225, 53803, 37441, 21134)
+
+# dx
+dx <- c(867, 147, 76, 95, 481, 630, 553, 659, 904, 1393, 2258, 3225, 4867, 7467,
+        10152, 12422, 16362, 16307, 21134)
+
+# nLx
+nLx <- c(99192, 396183, 494741, 494375, 492980, 490106, 487127, 484175, 480384,
+         474686, 465777, 452188, 432096, 401480, 357713, 301224, 228404, 145182,
+         110889)
+
+# Tx
+Tx <- rev(cumsum(rev(nLx)))
+
+# ex
+ex <- Tx / lx
+
+# combine
+exampleLT2 <- data.table::data.table(
+  age_start, age_end, age_length, deaths, pop, mx, ax, qx, px, lx, dx, nLx, Tx, ex
+)
+
+# use radix (l0) 1 instead of 100,000
+cols <- c("lx", "dx", "nLx", "Tx")
+exampleLT2[ , (cols) := lapply(.SD, function(x) x / 100000), .SDcols = cols]
+
+# save
+usethis::use_data(exampleLT2, overwrite = TRUE)
