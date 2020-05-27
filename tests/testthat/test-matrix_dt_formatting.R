b8db8db466e67ac3c171033ccac3522f7aed090a
Charlton Callender <chacalle@uw.edu>
2020-05-27 15:40:51 -0700

    try explicitly setting indices to NULL in test

--- tests/testthat/test-matrix_dt_formatting.R
+++ tests/testthat/test-matrix_dt_formatting.R
@@ -3,12 +3,14 @@ testthat::test_that("check `matrix_dt_formatting functions work", {
   # check with age and sex-specific data
   output_matrix <- dt_to_matrix(thailand_initial_estimates$survival)
   output_dt <- matrix_to_dt(output_matrix, year_right_most_endpoint = 2000)
+  data.table::setindexv(thailand_initial_estimates$survival, NULL)
   testthat::expect_equal(thailand_initial_estimates$survival, output_dt)
 
   # check with non age or sex-specific data
   output_matrix <- dt_to_matrix(thailand_initial_estimates$srb,
                                 id_cols = c("year_start", "year_end"))
   output_dt <- matrix_to_dt(output_matrix, year_right_most_endpoint = 2000)
+  data.table::setindexv(thailand_initial_estimates$srb, NULL)
   testthat::expect_equal(thailand_initial_estimates$srb, output_dt)
 
   # check with non sex-specific data
@@ -17,6 +19,7 @@ testthat::test_that("check `matrix_dt_formatting functions work", {
                                             "age_start", "age_end"))
   output_dt <- matrix_to_dt(output_matrix, year_right_most_endpoint = 2000,
                             age_right_most_endpoint = 50)
+  data.table::setindexv(thailand_initial_estimates$asfr, NULL)
   testthat::expect_equal(thailand_initial_estimates$asfr, output_dt)
 
   # check with non age or sex-specific data
