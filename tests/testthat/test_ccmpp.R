6e57233c792d5e07db8c36198f9c45ffa36b6f33
Charlton Callender <chacalle@uw.edu>
2020-11-05 14:31:41 -0800

    make ccmpp function flexible to 'mx', 'ax' or 'qx' inputs

--- tests/testthat/test_ccmpp.R
+++ tests/testthat/test_ccmpp.R
@@ -18,7 +18,7 @@ test_that("test that `ccmpp` gives expected output", {
       years = seq(1960, 1995, 5),
       sexes = c("female", "male"),
       ages = seq(0, 80, 5),
-      ages_survival = seq(0, 85, 5),
+      ages_mortality = seq(0, 85, 5),
       ages_asfr = seq(15, 45, 5)
     )
   )
@@ -49,7 +49,51 @@ test_that("test that `ccmpp` gives expected output with immigration/emigration",
       years = seq(1960, 1995, 5),
       sexes = c("female", "male"),
       ages = seq(0, 80, 5),
-      ages_survival = seq(0, 85, 5),
+      ages_mortality = seq(0, 85, 5),
+      ages_asfr = seq(15, 45, 5)
+    )
+  )
+  assertable::assert_ids(
+    data = population,
+    id_vars = list(year = seq(1960, 2000, 5),
+                   sex = c("female", "male"),
+                   age_start = seq(0, 80, 5)),
+    quiet = T
+  )
+  testthat::expect_identical(
+    names(population),
+    c("year", "sex", "age_start", "age_end", "value")
+  )
+})
+
+test_that("test that `ccmpp` works with 'mx', 'ax', 'qx' inputs", {
+
+  # create rough inputs for mx and ax
+  lt <- copy(demCore::thailand_initial_estimates$survival)
+  # px approximately equal to survivorship ratio
+  lt[, qx := 1 - value]
+  lt[, value := NULL]
+  lt[is.infinite(age_end), qx := 1]
+  # assign default ax values
+  lt[, ax := 2.5]
+  id_cols <- c("year_start", "year_end", "sex", "age_start", "age_end")
+  demCore::lifetable(lt, id_cols = id_cols)
+
+  # add inputs for mx and ax
+  new_inputs <- copy(demCore::thailand_initial_estimates)
+  new_inputs$mx <- lt[, .SD, .SDcols = c(id_cols, "mx")]
+  setnames(new_inputs$mx, "mx", "value")
+  new_inputs$ax <- lt[, .SD, .SDcols = c(id_cols, "ax")]
+  setnames(new_inputs$ax, "ax", "value")
+  new_inputs$survival <- NULL
+
+  population <- ccmpp(
+    inputs = new_inputs,
+    settings = list(
+      years = seq(1960, 1995, 5),
+      sexes = c("female", "male"),
+      ages = seq(0, 80, 5),
+      ages_mortality = seq(0, 85, 5),
       ages_asfr = seq(15, 45, 5)
     )
   )
